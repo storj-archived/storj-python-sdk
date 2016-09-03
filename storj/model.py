2566ca1d879d1cca29bcaa309bc9386bb5d0919b
Pedro Salgado <steenzout@ymail.com>
2016-09-02 23:30:15 -0600

    improved models.

--- storj/model.py
+++ storj/model.py
@@ -4,34 +4,34 @@
 from datetime import datetime
 
 from pytz import utc
+from steenzout.object import Object
 from storj import BucketManager
-from storj.api import MetadiskApiError
 from storj.sdk import FileManager, BucketKeyManager, TokenManager, ShardManager
 
 
-class Bucket:
+class Bucket(Object):
+    """"""
 
-    def __init__(self, json_payload):
-        try:
-            self.id = json_payload['id']
-            self.name = json_payload['name']
-            self.status = json_payload['status']
-            self.user = json_payload['user']
-            self.created_at = json_payload['created']
-            self.storage = json_payload['storage']
-            self.transfer = json_payload['transfer']
-            self.authorized_public_keys = json_payload['pubkeys']
-        except KeyError as e:
-            raise MetadiskApiError(
-                'Field "{field}" not present in JSON payload'.format(
-                    field=e.args[0]))
+    def __init__(
+            self, id=None, name=None, status=None, user=None,
+            created=None, storage=None, transfer=None, pubkeys=None):
+        self.id = id
+        self.name = name
+        self.status = status
+        self.user = user
+        self.storage = storage
+        self.transfer = transfer
+        self.authorized_public_keys = pubkeys
 
         self.files = FileManager(bucket_id=self.id)
         self.authorized_public_keys = BucketKeyManager(
             bucket=self, authorized_public_keys=self.authorized_public_keys)
         self.tokens = TokenManager(bucket_id=self.id)
-        self.created_at = datetime.strptime(
-            self.created_at, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=utc)
+
+        if created is not None:
+            self.created = datetime.strptime(created, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=utc)
+        else:
+            self.created = None
 
     def __str__(self):
         return self.name
@@ -43,15 +43,19 @@ class Bucket:
         BucketManager.delete(bucket_id=self.id)
 
 
-class Token:
+class Token(Object):
 
-    def __init__(self, json_payload):
-        self.id = json_payload['token']
-        self.bucket_id = json_payload['bucket']
-        self.operation = json_payload['operation']
-        self.expires_at = json_payload['expires']
-        self.expires_at = datetime.strptime(
-            self.expires_at, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=utc)
+    def __init__(
+            self, token=None, bucket=None, operation=None, expires=None):
+        self.id = token
+        self.bucket_id = bucket
+        self.operation = operation
+        self.expires_at = expires
+
+        if expires is not None:
+            self.expires = datetime.strptime(expires, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=utc)
+        else:
+            self.expires = None
 
     def __str__(self):
         return self.id
@@ -61,14 +65,14 @@ class Token:
             operation=self.operation, id=self.id)
 
 
-class File:
+class File(Object):
 
-    def __init__(self, json_payload):
-        self.bucket_id = json_payload['bucket']
-        self.hash = json_payload['hash']
-        self.content_type = json_payload['mimetype']
-        self.name = json_payload['filename']
-        self.size = json_payload['size']
+    def __init__(self, bucket=None, hash=None, mimetype=None, filename=None, size=None):
+        self.bucket_id = bucket
+        self.hash = hash
+        self.content_type = mimetype
+        self.name = filename
+        self.size = size
         self.shardManager = ShardManager()
 
     def __str__(self):
@@ -79,8 +83,7 @@ class File:
             name=self.name, size=self.size, content_type=self.content_type)
 
     def download(self):
-        return api_client.download_file(
-            bucket_id=self.bucket_id, file_hash=self.hash)
+        return api_client.download_file(bucket_id=self.bucket_id, file_hash=self.hash)
 
     def delete(self):
         bucket_files = FileManager(bucket_id=self.bucket_id)

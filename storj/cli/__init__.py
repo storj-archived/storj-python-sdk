4c7c3775b002a98477e74651fbc42901b71d31a4
Pedro Salgado <steenzout@ymail.com>
2016-09-02 23:15:07 -0600

    added more content to the cli module.

--- storj/cli/__init__.py
+++ storj/cli/__init__.py
@@ -2,3 +2,24 @@
 """Storj command-line interface package."""
 
 import click
+
+from storj.api import generate_new_key_pair, export_keys, import_keys
+
+@click.group
+def cli():
+    pass
+
+
+@click.command
+def generate_keys():
+    pass
+
+
+@click.command
+def export_keys():
+    pass
+
+
+@click.command
+def import_keys():
+    pass

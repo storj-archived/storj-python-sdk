2ccc3d5c12f36c7886afcf2553178715bdb99452
Pedro Salgado <steenzout@ymail.com>
2016-09-03 22:08:19 -0600

    added logging and configuration for integration tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,69 @@
+# -*- coding: utf-8 -*-
+"""Storj tests."""
+
+import logging
 import unittest
 
 
-if __name__ == "__main__":
-    unittest.main()
+from abc import ABCMeta
+
+from . import config
+from . import log_config
+
+
+config.load_configuration()
+log_config.load_configuration()
+
+
+class Basic(object):
+    """Basic functionality to enhance test cases.
+
+    Attributes:
+        configuration ():
+        logger (): logger.
+    """
+    __metaclass__ = ABCMeta
+
+    def setup_configuration(self):
+        """
+        Setup test configuration.
+        It will also load (once) the test configuration.
+        """
+        logging.getLogger('%s.%s' % (__name__, 'Basic')).info('setup_configuration()')
+
+        self.configuration = config.get()
+
+    def setup_logger(self):
+        """
+        Setup test logger.
+        It will also load (once) the test logging configuration.
+        """
+        logging.getLogger('%s.%s' % (__name__, 'Basic')).info('setup_logger()')
+
+        self.logger = logging.getLogger('%s.%s' % (__name__, self.__class__.__name__))
+
+
+class AbstractTestCase(unittest.TestCase, Basic):
+    """Base test case."""
+    __metaclass__ = ABCMeta
+
+    __slots__ = ('configuration', 'logger')
+
+    def __init__(self, methodName):
+        """Initializes a BaseTestCase instance.
+
+        Args:
+            methodName (str): the test method to be executed.
+        """
+        super(AbstractTestCase, self).__init__(methodName)
+
+        self.setup_logger()
+        self.setup_configuration()
+
+    def setUp(self):
+        """Setup test resources."""
+        self.logger.info('setUp()')
+
+    def tearDown(self):
+        """Tear down test resources."""
+        self.logger.info('tearDown()')
---
+++ tests/config.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""Test configuration module."""
+
+from __future__ import absolute_import
+
+import os.path
+
+try:
+    from ConfigParser import SafeConfigParser as ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+
+import logging
+
+
+DEFAULT_CONFIG_FILE = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'storj.cfg')
+
+
+class Cache(object):
+    """
+    Abstract class to cache configuration settings.
+    """
+    settings = None
+
+
+def load_configuration(config_file=DEFAULT_CONFIG_FILE):
+    """
+    Loads configuration.
+
+    :param config_file: the configuration file(s).
+    :type config_file: str or list of str
+    """
+    if not os.path.exists(config_file) or not os.path.isfile(config_file):
+        msg = '%s configuration file does not exist!' % config_file
+        logging.getLogger(__name__).error(msg)
+        raise ValueError(msg)
+
+    parser = ConfigParser()
+    try:
+        parser.read(config_file)
+        Cache.settings = {}
+        for section in parser.sections():
+            Cache.settings[section] = dict(parser.items(section))
+        logging.getLogger(__name__).info('%s configuration file was loaded.', config_file)
+        return Cache.settings
+    except Exception as e:
+        Cache.settings = None
+        logging.getLogger(__name__).error('Failed to load configuration from %s!', config_file)
+        logging.getLogger(__name__).debug(str(e), exc_info=True)
+        raise e
+
+
+def get():
+    """
+    Returns the configuration.
+
+    :return: the configuration.
+    :rtype: object (configParser.ConfigParser)
+    """
+    if Cache.settings is None:
+        return load_configuration()
+    return Cache.settings
+
+
+def reset():
+    """
+    Reset the configuration.
+    """
+    Cache.settings = None

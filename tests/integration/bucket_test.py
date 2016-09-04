09d57082c2713f965c9577a51247c7af17d54b54
Pedro Salgado <steenzout@ymail.com>
2016-09-03 23:36:01 -0600

    added more debug logging. fixed test for empty generator.

--- tests/integration/bucket_test.py
+++ tests/integration/bucket_test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Storj API bucket endpoint integration tests."""
 
+import pytest
+
+
 from . import Integration
 
 
@@ -15,7 +18,7 @@ class Bucket(Integration):
         """Setup test bucket."""
         super(Bucket, self).setUp()
 
-        self.bucket = self.client.bucket_create('integration-%s' % self.test_id)
+        # self.bucket = self.client.bucket_create('integration-%s' % self.test_id)
 
     def tearDown(self):
         """Destroy test bucket."""
@@ -33,12 +36,20 @@ class Bucket(Integration):
         1.1 get does not retrieve the bucket
         1.2 list does not have bucket
         """
+        self.logger.debug('---------- %s.test() ----------' % __name__)
+
+        self.logger.debug('%s 1.1' % __name__)
         assert self.bucket == self.client.bucket_get(self.bucket.id)
 
+        self.logger.debug('%s 1.2' % __name__)
         assert self.bucket in self.client.bucket_list()
 
+        self.logger.debug('%s 2.' % __name__)
         self.client.bucket_delete(self.bucket.id)
 
+        self.logger.debug('%s 2.1' % __name__)
         assert self.client.bucket_get(self.bucket.id) is None
 
-        assert [] == self.client.bucket_list()
+        self.logger.debug('%s 2.2' % __name__)
+        with pytest.raises(StopIteration):
+            self.client.bucket_list().next()

e12108c2b3b94d6fcff273e17a447bbc47288581
Pedro Salgado <steenzout@ymail.com>
2016-09-03 23:02:35 -0600

    improved docstring. improved error handling. added setUp().

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,31 +1,44 @@
 # -*- coding: utf-8 -*-
 """Storj API integration tests."""
 
-import sys
 import os
 
-from abc import ABCMeta
+from datetime import datetime
+from storj import http
 
 from .. import AbstractTestCase
 
 
+#: name of the environment variable that contains the Storj account email.
 ENV_STORJ_EMAIL = 'STORJ_EMAIL'
+#: name of the environment variable that contains the Storj account password.
 ENV_STORJ_PASSWORD = 'STORJ_PASSWORD'
 
 
 class Integration(AbstractTestCase):
-    """Abstract class for integration tests."""
-    __metaclass__ = ABCMeta
+    """Base class for integration tests.
+
+    Attributes:
+        email (str): Storj account email.
+        password (str): Storj account password.
+        client (:py:class:`storj.http.Client`): HTTP client.
+        test_id (str): unique identifier to be appended to temporary resources.
+    """
 
     def __init__(self, methodName):
-        super(AbstractTestCase, self).__init__(methodName)
+        super(Integration, self).__init__(methodName)
 
         try:
             self.email = os.environ[ENV_STORJ_EMAIL]
             self.password = os.environ[ENV_STORJ_PASSWORD]
         except KeyError as e:
             self.logger.error(e)
-            self.logger.error(
-                'To run integration tests you need to define the following environment variables: %s, %s.' % (
-                    ENV_STORJ_EMAIL, ENV_STORJ_PASSWORD))
-            sys.exit(os.EX_CONFIG)
+            err_msg = 'To run integration tests you need to define the following environment variables: %s, %s.' % (
+                    ENV_STORJ_EMAIL, ENV_STORJ_PASSWORD)
+            self.logger.error(err_msg)
+            self.fail(err_msg)
+
+    def setUp(self):
+        super(Integration, self).setUp()
+        self.client = http.Client(self.email, self.password)
+        self.test_id = datetime.utcnow().strftime('%Y%m%d_%H%M%S')

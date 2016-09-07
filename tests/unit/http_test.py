13a95727ebf9e1bfc4c3daed6f2e0fe0cede606b
Pedro Salgado <steenzout@ymail.com>
2016-09-06 18:26:59 -0600

    added test stub functions.

--- tests/unit/http_test.py
+++ tests/unit/http_test.py
@@ -33,3 +33,99 @@ class ClientTestCase(AbstractTestCase):
         assert 'Authorization' in request_kwargs['headers']
         assert request_kwargs['headers']['Authorization'].startswith('Basic ')
         assert request_kwargs['headers']['Authorization'].endswith('==')
+
+    def test_bucket_create(self):
+        """Test Client.bucket_create()."""
+        pass
+
+    def test_bucket_delete(self):
+        """Test Client.bucket_delete()."""
+        pass
+
+    def test_bucket_files(self):
+        """Test Client.bucket_files()."""
+        pass
+
+    def test_bucket_get(self):
+        """Test Client.bucket_get()."""
+        pass
+
+    def test_bucket_list(self):
+        """Test Client.bucket_list()."""
+        pass
+
+    def test_bucket_set_keys(self):
+        """Test Client.bucket_set_keys()."""
+        pass
+
+    def test_contact_list(self):
+        """Test Client.contact_list()."""
+        pass
+
+    def test_file_download(self):
+        """Test Client.file_download()."""
+        pass
+
+    def test_file_get(self):
+        """Test Client.file_get()."""
+        pass
+
+    def test_file_upload(self):
+        """Test Client.file_upload()."""
+        pass
+
+    def test_frame_add_shard(self):
+        """Test Client.frame_add_shard()."""
+        pass
+
+    def test_frame_create(self):
+        """Test Client.frame_create()."""
+        pass
+
+    def test_frame_delete(self):
+        """Test Client.frame_delete()."""
+        pass
+
+    def test_frame_get(self):
+        """Test Client.frame_get()."""
+        pass
+
+    def test_frame_list(self):
+        """Test Client.frame_list()."""
+        pass
+
+    def test_key_delete(self):
+        """Test Client.key_delete()."""
+        pass
+
+    def test_key_dump(self):
+        """Test Client.key_dump()."""
+        pass
+
+    def test_key_export(self):
+        """Test Client.key_export()."""
+        pass
+
+    def test_key_generate(self):
+        """Test Client.key_generate()."""
+        pass
+
+    def test_key_get(self):
+        """Test Client.key_get()."""
+        pass
+
+    def test_key_import(self):
+        """Test Client.key_import()."""
+        pass
+
+    def test_key_register(self):
+        """Test Client.key_register()."""
+        pass
+
+    def test_token_create(self):
+        """Test Client.token_create()."""
+        pass
+
+    def test_user_create(self):
+        """Test Client.user_create()."""
+        pass

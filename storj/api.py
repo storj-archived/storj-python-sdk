2bdec6934c41e8b994d6d16f25e554f7015bcf96
Pedro Salgado <steenzout@ymail.com>
2016-09-07 10:32:15 -0600

    removed unnecessary imports.

--- storj/api.py
+++ storj/api.py
@@ -3,19 +3,6 @@
 
 from binascii import b2a_hex
 
-try:
-    from json.decoder import JSONDecodeError
-except ImportError:
-    # Python 2
-    JSONDecodeError = ValueError
-
-try:
-    from urllib.parse import urljoin, urlencode
-except ImportError:
-    # Python 2
-    from urllib import urlencode
-    from urlparse import urljoin
-
 
 def ecdsa_to_hex(ecdsa_key):
     """

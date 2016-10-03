119b31b8524a11ea58c968f9140bf370adf85876
James Prestwich <James@storj.io>
2016-10-02 18:10:02 -0600

    add author email

--- storj/metadata.py
+++ storj/metadata.py
@@ -2,5 +2,5 @@
 """Storj metadata module."""
 
 __author__ = 'Daniel Hawkins,Fabian Barkhau,Mike Bailey,Pedro Salgado,James Prestwich'
-__author_email__ = 'hwkns@alum.mit.edu;f483@storj.io;mibgranny@aol.com;steenzout@ymail.com'
+__author_email__ = 'hwkns@alum.mit.edu;f483@storj.io;mibgranny@aol.com;steenzout@ymail.com;james@storj.io'
 __version__ = '0.1.5'

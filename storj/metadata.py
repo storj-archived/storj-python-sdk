505c1897e6579fad0e6ee7c89ce3488f540f0a9b
Marc Schubert <marc.schubert@veenkergmbh.de>
2016-12-13 10:35:41 +0100

    from core.lib.utils migrate simpleDecrypt and simpleEncrypt of data for id_ecdsa

--- storj/metadata.py
+++ storj/metadata.py
@@ -2,13 +2,14 @@
 """Storj metadata module."""
 
 __author__ = (
-    'Daniel Hawkins,Fabian Barkhau,Mike Bailey,Pedro Salgado,James Prestwich'
+    'Daniel Hawkins,Fabian Barkhau,Mike Bailey,Pedro Salgado,James Prestwich,Marc Schubert'
 )
 __author_email__ = (
     'hwkns@alum.mit.edu;'
     'f483@storj.io;'
     'mibgranny@aol.com;'
     'steenzout@ymail.com;'
-    'james@storj.io'
+    'james@storj.io;'
+    'marc.schubert@gmail.com'
 )
 __version__ = '0.1.6'

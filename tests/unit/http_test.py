37639847a3158479bc3f593f97a1075531b7e6aa
Wiktor Jezioro <wikjezioro@op.pl>
2017-04-22 15:19:39 +0200

    Fix file_pointers tests

--- tests/unit/http_test.py
+++ tests/unit/http_test.py
@@ -247,6 +247,8 @@ class ClientTestCase(AbstractTestCase):
         """Test Client.file_pointers()."""
         test_bucket_id = '1234'
         test_file_id = '5678'
+        test_limit = '10'
+        test_skip = '0'
 
         mock_token_create.return_value = model.Token(token='test_token')
 
@@ -258,9 +260,8 @@ class ClientTestCase(AbstractTestCase):
 
         self.mock_request.assert_called_once_with(
             method='GET',
-            path='/buckets/%s/files/%s/' % (test_bucket_id, test_file_id),
-            headers={'x-token': None, 'skip': 'None', 'limit': 'None'},
-            json={'skip': 'None', 'limit': 'None'})
+            path='/buckets/%s/files/%s/?skip=%s&limit=%s' % (test_bucket_id, test_file_id, test_skip, test_limit),
+            headers={'x-token': pull_token.id})
 
         mock_token_create.assert_called_once_with(
             test_bucket_id, operation='PULL')

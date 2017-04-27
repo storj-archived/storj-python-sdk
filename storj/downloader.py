2cba6446397fdd1515f64684ab7269dd7c7c2e9d
Marco Rosa <marcor165@hotmail.it>
2017-04-28 00:21:37 +0200

    generate strings with %s instead of +

--- storj/downloader.py
+++ storj/downloader.py
@@ -38,7 +38,7 @@ class Downloader:
             file_metadata = self.client.file_metadata(bucket_id, file_id)
             # Get file name
             self.filename_from_bridge = str(file_metadata.filename)
-            self.__logger.debug("Filename from bridge: " +
+            self.__logger.debug("Filename from bridge: %s" %
                                 self.filename_from_bridge)
             # Get file frame
             self.file_frame = file_metadata.frame
@@ -71,17 +71,17 @@ class Downloader:
         self.all_shards_count = self.get_file_pointers_count(bucket_id, file_id)
         # Set the paths
         self.tmp_path, self.destination_file_path = self.get_paths()
-        self.__logger.debug("temp path " + self.tmp_path)
-        self.__logger.debug("destination path " + self.destination_file_path)
+        self.__logger.debug("temp path %s" % self.tmp_path)
+        self.__logger.debug("destination path %s" % self.destination_file_path)
 
         mp = Pool()
         try:
             self.__logger.debug("Resolving file pointers to download file \
-                    with ID: " + str(file_id) + "...")
+                    with ID: %s ..." % file_id)
 
             tries_get_file_pointers = 0
             while MAX_RETRIES_GET_FILE_POINTERS > tries_get_file_pointers:
-                self.__logger.debug("Attempt number %d of getting a pointer \
+                self.__logger.debug("Attempt number %s of getting a pointer \
                     to the file" % tries_get_file_pointers)
                 tries_get_file_pointers += 1
                 try:
@@ -91,7 +91,7 @@ class Downloader:
                         file_id,
                         limit=str(self.all_shards_count),
                         skip="0")
-                    self.__logger.debug("There are %d shard pointers: " %
+                    self.__logger.debug("There are %s shard pointers: " %
                                         len(shard_pointers))
 
                     self.__logger.debug("Begin shards download process")
@@ -100,7 +100,7 @@ class Downloader:
                 except exception.StorjBridgeApiError as e:
                     self.__logger.error("Bridge error")
                     self.__logger.error("Error while resolving file pointers \
-                        to download file with ID: " + str(file_id) + "...")
+                        to download file with ID: %s ..." % file_id)
                     self.__logger.error(e)
                     continue
                 else:
@@ -109,7 +109,7 @@ class Downloader:
         except exception.StorjBridgeApiError as e:
             self.__logger.error("Outern Bridge error")
             self.__logger.error("Error while resolving file pointers to \
-                download file with ID: " + str(file_id))
+                download file with ID: %s" % str(file_id))
 
         # All the shards have been downloaded
         self.finish_download()
@@ -130,11 +130,11 @@ class Downloader:
         actual_path = os.path.join(self.tmp_path, self.filename_from_bridge)
         destination_path = os.path.join(self.destination_file_path,
                                         self.filename_from_bridge)
-        self.__logger.debug("Actual path " + actual_path)
-        self.__logger.debug("Destination path " + destination_path)
+        self.__logger.debug("Actual path %s" % actual_path)
+        self.__logger.debug("Destination path %s" % destination_path)
         if fileisencrypted:
             sharing_tools.join_shards(actual_path, "-",
-                                      actual_path + ".encrypted")
+                                      "%s.encrypted" % actual_path)
         else:
             sharing_tools.join_shards(actual_path, "-", destination_path)
 
@@ -144,7 +144,7 @@ class Downloader:
             file_crypto_tools = FileCrypto()
             # Begin file decryption
             file_crypto_tools.decrypt_file("AES",
-                                           actual_path + ".encrypted",
+                                           "%s.encrypted" % actual_path,
                                            destination_path,
                                            str(self.client.password))
 
@@ -154,11 +154,11 @@ class Downloader:
         # Remove temp files
         try:
             # Remove shards
-            file_shards = map(lambda i: actual_path + "-" + str(i),
+            file_shards = map(lambda i: "%s-%s" % (actual_path, i),
                               range(self.all_shards_count))
             map(os.remove, file_shards)
             # Remove encrypted file
-            os.remove(actual_path + ".encrypted")
+            os.remove("%s.encrypted" % actual_path)
         except OSError as e:
             self.__logger.error(e)
         return True
@@ -166,8 +166,8 @@ class Downloader:
     def create_download_connection(self, url, path_to_save, shard_index):
         farmer_tries = 0
 
-        self.__logger.debug("Downloading shard at index " +
-                            str(shard_index) + " from farmer: " + str(url))
+        self.__logger.debug("Downloading shard at index %s from farmer: %s" %
+                            (shard_index, url))
 
         tries_download_from_same_farmer = 0
         while MAX_RETRIES_DOWNLOAD_FROM_SAME_FARMER > \
@@ -185,14 +185,14 @@ class Downloader:
                     raise exception.StorjFarmerError()
             except exception.StorjFarmerError as e:
                 self.__logger.error(e)
-                self.__logger.error("First try failed. Retrying... (" +
-                                    str(farmer_tries) + ")")  # update shard download state
+                self.__logger.error("First try failed. Retrying... (%s)" %
+                                    str(farmer_tries))  # update shard download state
                 continue
             except Exception as e:
                 self.__logger.error("Unhandled error")
                 self.__logger.error("Error occured while downloading shard \
-                    at index " + str(shard_index) + ". Retrying... (" +
-                                    str(farmer_tries) + ")")
+                    at index %s. Retrying... (%s)" % (shard_index, farmer_tries)
+                                    )
                 self.__logger.error(e)
                 continue
             else:
@@ -209,8 +209,7 @@ class Downloader:
             #     raise IOError("13")
 
             self.__logger.debug("Starting download threads...")
-            self.__logger.debug("Downloading shard at index " +
-                                str(shard_index) + "...")
+            self.__logger.debug("Downloading shard at index %s ..." % shard_index)
 
             url = "http://%s:%s/shards/%s?token=%s" % (
                 pointer.get('farmer')['address'],
@@ -233,12 +232,12 @@ class Downloader:
             self.create_download_connection(url, file_temp_path, shard_index)
 
             self.__logger.debug("Shard downloaded")
-            self.__logger.debug("Shard at index " + str(shard_index) +
-                                " downloaded successfully.")
-            self.__logger.debug(file_temp_path + " saved")
+            self.__logger.debug("Shard at index %s downloaded successfully" %
+                                shard_index)
+            self.__logger.debug("%s saved" % file_temp_path)
 
         except IOError as e:
-            self.__logger.error("Perm error " + str(e))
+            self.__logger.error("Perm error %s" % str(e))
             if str(e) == str(13):
                 self.__logger.error("""Error while saving or reading file or
                 temporary file.

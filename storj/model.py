1b9b5b68ea2e998b0676ff9908dfe871c94a5545
Wiktor Jezioro <wikjezioro@op.pl>
2017-05-23 21:14:32 +0200

    Add missing index attribute

--- storj/model.py
+++ storj/model.py
@@ -129,13 +129,14 @@ class File(Object):
     """
 
     def __init__(self, bucket=None, hash=None, mimetype=None,
-                 filename=None, size=None, id=None, frame=None, created=None, hmac=None, erasure=None):
+                 filename=None, size=None, id=None, frame=None, created=None, hmac=None, erasure=None, index=None):
         self.bucket = Bucket(id=bucket)
         self.hash = hash
         self.mimetype = mimetype
         self.filename = filename
         self.size = size
-        self.erasure = erasure
+        self.erasure = index
+        self.index = erasure
         self.shard_manager = None
         self.id = id
         self.frame = Frame(id=frame)
